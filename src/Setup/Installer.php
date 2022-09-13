a3604817628273edd9ba4d890b6084e1bbd971ad
lijin2hats <lijin@2hatslogic.com>
2022-09-13 10:10:42 +0530

    footer custom snippets

--- src/Setup/Installer.php
+++ src/Setup/Installer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace StwElleThemeChanges\Setup;
 
 use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Uuid\Uuid;
 use StwElleThemeChanges\Setup\Helper;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
@@ -42,8 +43,15 @@ class Installer
         $this->container = $container;
     }
 
+    public function createCustomerCustomFields()
+    {
+        $this->customFieldsForSalesChannel();
+    }
+
     public function install()
     { 
+        $this->createCustomerCustomFields();
+
         $elleCustomMenuIcons = $this->fetchCustomFieldSetId('elle_custom_menu_icons');
         if (!$elleCustomMenuIcons) {
             $repo = $this->container->get('custom_field_set.repository');
@@ -51,8 +59,9 @@ class Installer
             $repo->create([$attributeSet], Context::createDefaultContext());
         }
 
-        
         return false;
+
+        
     }
 
     private function fetchCustomFieldSetId($technicalName)
@@ -68,4 +77,72 @@ class Installer
 
         return ($customFieldSetDetails) ? $customFieldSetDetails->getId() : null;
     }
+
+    private function customFieldsForSalesChannel(): void
+    {
+        $customFieldSetId = $this->fetchCustomFieldSetId('elle_custom_footerSettings');
+        if (!$customFieldSetId) {
+            $repo = $this->container->get('custom_field_set.repository');
+            $id = Uuid::randomHex();
+            $attributeSet = [
+                'id' => $id,
+                'name' => 'elle_custom_footerSettings',
+                'config' => ['label' => [
+                    'en-GB' => 'Footer Snippets',
+                    'de-DE' => 'Fußzeilenausschnitte',
+                ]],
+                'customFields' => [
+                    [
+                        'id' => Uuid::randomHex(),
+                        'name' => 'elle_custom_footer_snippet_1',
+                        'type' => 'text',
+                        'config' => [
+                            'label' => [
+                                'en-GB' => 'footer snippet one',
+                                'de-DE' => 'Fußzeilenausschnitt eins',
+                            ],
+                            'componentName' => 'sw-textarea-field',
+                            'customFieldType' => 'textarea',
+                            'customFieldPosition' => 1,
+                        ],
+                    ],
+                    [
+                        'id' => Uuid::randomHex(),
+                        'name' => 'elle_custom_footer_snippet_2',
+                        'type' => 'text',
+                        'config' => [
+                            'label' => [
+                                'en-GB' => 'footer snippet two',
+                                'de-DE' => 'Fußzeilenausschnitt zwei',
+                            ],
+                            'componentName' => 'sw-textarea-field',
+                            'customFieldType' => 'textarea',
+                            'customFieldPosition' => 2,
+                        ],
+                    ],
+                    [
+                        'id' => Uuid::randomHex(),
+                        'name' => 'elle_custom_footer_snippet_3',
+                        'type' => 'text',
+                        'config' => [
+                            'label' => [
+                                'en-GB' => 'footer snippet three',
+                                'de-DE' => 'Fußzeilenausschnitt drei',
+                            ],
+                            'componentName' => 'sw-textarea-field',
+                            'customFieldType' => 'textarea',
+                            'customFieldPosition' => 3,
+                        ],
+                    ],
+                ],
+                'relations' => [
+                    [
+                        'entityName' => 'sales_channel',
+                    ],
+                ],
+            ];
+            $result = $repo->create([$attributeSet], Context::createDefaultContext());
+        }
+    }
+
 }
